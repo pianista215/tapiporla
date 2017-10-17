aa1befe6022a53d1b0de524c5b5e7203431badc2
pianista215 <unaisarasola@hotmail.com>
2017-10-17 19:57:56 +0200

    Removed references to hotel example

--- pocs/crawlerElEconomista/ibex35ElEconomista/ibex35ElEconomista/spiders/ibex35_spider.py
+++ pocs/crawlerElEconomista/ibex35ElEconomista/ibex35ElEconomista/spiders/ibex35_spider.py
@@ -11,24 +11,12 @@ class HotelsSpider(scrapy.Spider):
     def parse(self, response):
         for ibex_row_data in response.css('table[class*=tablehistoricos]').css("tbody").css("tr")
             cols = ibex_close_data.css("td").extract()
+            if #Check is not an "information row"
             date =
             close_value =
             max_value =
             min_value =
-            name = hotel.css(".sr-hotel__name::text").extract_first().strip()
-            stars = hotel.css("::attr(data-class)").extract_first().strip()
-            price = hotel.css('strong').css("b::text").extract_first()
-            image_url = hotel.css('img::attr(src)').extract_first().strip()
-            if price is not None:
-                #Avoid € storage
-                price_value = price.strip()[2:]
-                yield {
-                    'city': city,
-                    'name': name,
-                    'stars': int(stars),
-                    'price': float(price_value),
-                    'image_url': image_url
-                }
+            
 
         #next_page = response.css('[data-page-next]::attr(href)').extract_first()
         #if next_page is not None:
