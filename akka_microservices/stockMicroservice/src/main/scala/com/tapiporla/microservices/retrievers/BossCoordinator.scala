b8869f154b10f3a6111e9d5229279dee7f12841b
Unai Sarasola <pianista215@users.noreply.github.com>
2017-11-22 12:49:31 +0100

    Support more than one stock passed in config (#16)
    * Renamed project
    
    * Docker support
    
    * Environmental variables
    
    * Boss coordinator supports multistock to retrieve
    
    * Unhandled message

---
+++ akka_microservices/stockMicroservice/src/main/scala/com/tapiporla/microservices/retrievers/BossCoordinator.scala
@@ -0,0 +1,43 @@
+package com.tapiporla.microservices.retrievers
+
+import akka.actor.{ActorRef, Props}
+import com.tapiporla.microservices.retrievers.BossCoordinator.StartAllCoordinators
+import com.tapiporla.microservices.retrievers.common.{TapiporlaActor, TapiporlaConfig}
+import com.tapiporla.microservices.retrievers.indices.stock.{StockCoordinator, StockHistoricManager}
+
+object BossCoordinator {
+
+  def props():Props = Props(new BossCoordinator())
+
+  case object StartAllCoordinators
+}
+
+class BossCoordinator extends TapiporlaActor{
+
+
+  override def receive = initial
+
+  def initial: Receive = {
+
+    case StartAllCoordinators =>
+      log.info("Initializing BossCoordinator")
+
+      val stockCoordinatorsToLaunch = TapiporlaConfig.Stock.stocks
+      log.info(s"Found StockCoordinators: $stockCoordinatorsToLaunch")
+
+      //The actors are autoinitialiced on creation
+      val actors = stockCoordinatorsToLaunch.map {stockConfig =>
+        context.actorOf(StockCoordinator.props(stockConfig), name = s"${stockConfig.name}StockCoordinator")
+      }
+
+      context.become(started(actors))
+
+
+  }
+
+  def started(coordinatorsLaunched: Seq[ActorRef]): Receive = {
+    case message => unhandled(message)
+  }
+
+
+}
--- akka_microservices/stockMicroservice/src/main/scala/com/tapiporla/microservices/retrievers/Main.scala
+++ akka_microservices/stockMicroservice/src/main/scala/com/tapiporla/microservices/retrievers/Main.scala
@@ -1,15 +1,16 @@
 package com.tapiporla.microservices.retrievers
 
-import akka.actor.{ActorSystem, Props}
-import com.tapiporla.microservices.retrievers.indices.stock.StockCoordinator
+import akka.actor.ActorSystem
+import com.tapiporla.microservices.retrievers.BossCoordinator.StartAllCoordinators
 
 object Main {
 
   def main(args: Array[String]): Unit = {
 
     val system = ActorSystem("retrievers")
-    val testRetriever = system.actorOf(StockCoordinator.props())
+    val bossCoordinator = system.actorOf(BossCoordinator.props())
 
+    bossCoordinator ! StartAllCoordinators
   }
 
 }
