0bde81b607da38415de66fe2c0e3c15dfff3a7ab
Unai <usarasola@stratio.com>
2019-10-04 12:13:47 +0200

    calculate ema

--- akka_microservices/stockMicroservice/src/main/scala/com/tapiporla/microservices/retrievers/common/stats/StatsUtils.scala
+++ akka_microservices/stockMicroservice/src/main/scala/com/tapiporla/microservices/retrievers/common/stats/StatsUtils.scala
@@ -8,4 +8,9 @@ object StatsUtils {
   def mean(data: Seq[BigDecimal]): BigDecimal =
     data.sum / data.length
 
+  def ema(currentDay: BigDecimal, previousEma: BigDecimal, n: Int): BigDecimal = {
+    val k = 2 / (n.toDouble + 1)
+    currentDay * k + previousEma * (1-k)
+  }
+
 }
