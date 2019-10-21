45bbcc88016388582854bd9af48909708896e524
Unai <usarasola@stratio.com>
2019-10-21 17:56:58 +0200

    Median

--- akka_microservices/stockMicroservice/src/main/scala/com/tapiporla/microservices/retrievers/common/stats/StatsUtils.scala
+++ akka_microservices/stockMicroservice/src/main/scala/com/tapiporla/microservices/retrievers/common/stats/StatsUtils.scala
@@ -8,6 +8,15 @@ object StatsUtils {
   def mean(data: Seq[BigDecimal]): BigDecimal =
     data.sum / data.length
 
+  def median(data: Seq[BigDecimal]): BigDecimal = {
+    val sortedSeq = data.sortWith(_ < _)
+    if (data.size % 2 == 1) sortedSeq(sortedSeq.size / 2)
+    else {
+      val (up, down) = sortedSeq.splitAt(data.size / 2)
+      (up.last + down.head) / 2
+    }
+  }
+
   def ema(currentDay: BigDecimal, previousEma: BigDecimal, n: Int): BigDecimal = {
     val k = 2 / (n.toDouble + 1)
     currentDay * k + previousEma * (1-k)
